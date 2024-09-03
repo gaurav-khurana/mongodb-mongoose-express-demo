0c5a7d94a04e9a0d9e29d636f1006c3355107aae
Gaurav <program.drgk@gmail.com>
2024-09-03 07:22:37 -0700

    User schema modified

--- user.js
+++ user.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  email: String,
+  createdAt: Date,
+  updatedAt: Date,
   hobbies: [String],
   address: {
     street: String,
     city: String,
   },
+  bestFriend: mongoose.SchemaTypes.ObjectId,
 });
 
 module.exports = mongoose.model("users", userSchema);
