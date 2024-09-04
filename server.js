00853173df2907c366288ce22ee298b755f5809f
Gaurav <program.drgk@gmail.com>
2024-09-03 23:20:18 -0700

    userSchema modified; validation added

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
-const user = require("./user");
+const User = require("./user");
 
 mongoose.connect("mongodb://localhost:27017/appdb");
 
 console.log("Connected to MongoDB");
 
 async function runMongoDB() {
-  // chk if cn read from appdb collection frm mongodb
-  let user1 = await user.find({ name: "Kris" });
-  user1 = await user.find({ age: { $gt: 12 } });
-  console.log(user1);
+  try {
+    const newUser = await User.create({
+      name: "Piastri",
+      age: 31,
+      email: "pk123@gmail.com",
+      hobbies: ["volleyball", "hiking", "gaming"],
+      address: {
+        street: "123 Garden city",
+        city: "Vancouver",
+      },
+    });
+    console.log("User created", newUser);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 runMongoDB();
--- user.js
+++ user.js
@@ -1,16 +1,53 @@
 const mongoose = require("mongoose");
 
+// const addressSchema = mongoose.Schema({
+//   street: String,
+//   city: String,
+// });
+
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
-  email: String,
-  createdAt: Date,
-  updatedAt: Date,
+
+  age: {
+    type: Number,
+    min: 1, // can set range, this prevents -ve value or invalid value
+    max: 100,
+    validate: {
+      // custom validation
+      validator: (value) => value % 2 === 0, // to chk if age is even number
+      message: (props) => `${props.value} is not an even number`, // msg to render if validation fails
+    },
+  },
+
+  email: {
+    type: String,
+    required: true, // makes field mandatory
+    lowercase: true, // converts email to lowercase; if uppercase: true then all uppercase
+    minLength: 10, // adds validation for str with min & max length
+    maxLength: 50,
+  },
+
+  createdAt: {
+    type: Date,
+    default: () => Date.now(), // runs func to get current time & date; if default: Date then static value
+    immutable: true, // immutable means cant change createdAt field manually
+  },
+
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+
   hobbies: [String],
+
+  // can have address like this for simple nesting; else have seperate schema & ref here
   address: {
     street: String,
     city: String,
   },
+  // ref above schema here
+  // address: addressSchema,
+
   bestFriend: mongoose.SchemaTypes.ObjectId,
 });
 
