86dc649b7faec6eafe214c8c9143f156be68b125
Gaurav <program.drgk@gmail.com>
2024-09-04 00:13:04 -0700

    Custom queries in Mongoose

--- server.js
+++ server.js
@@ -21,29 +21,54 @@ async function runMongoDB() {
     console.log("User created", newUser);
 
     // update user
-    let findUser = await User.findById("66d7fc95b441f14144184c54");
+    let findUser = await User.findById("66d803f8b182d62618808a5d");
     findUser.email = "piastri@f1.com";
-    findUser.save();
+    await findUser.save();
 
     // query db
     console.log(await User.find()); // to find all users
 
-    let user = await User.findById("66d7fc95b441f14144184c54"); // findById pass user.id
+    let user = await User.findById("66d803f8b182d62618808a5d"); // findById pass user.id
 
     user = await User.find({ name: "Piastri" }); // gives all user with this name
+    console.log(user);
 
     user = await User.findOne({ name: "Piastri" }); // gives 1st user with this name
+    console.log(user);
 
-    user = await User.exists({ name: "Piastri" }); // boolean output to see if users exists
+    user = await User.exists({ name: "Piastri" }); // chk if users exists
 
-    user = await User.deleteOne({ name: "Piastri" }); // Deletes 1st user with this name
+    // user = await User.deleteOne({ name: "Piastri" }); // Deletes 1st user with this name
 
-    user = await User.deleteMany({ name: "Piastri" }); // Deletes all user with this name
+    // user = await User.deleteMany({ name: "Piastri" }); // Deletes all user with this name
+
+    console.log(user);
+
+    // custom query method in mongoose
+    user = await User.where("name").equals("Piastri"); // same as .find({name: 'Piastri'})
+
+    user = await User.where("age").gt("40"); // filter results with age > 40
+
+    user = await User.where("age").lt("30"); // filter results with age < 30
+
+    user = await User.where("age").lt("30").where("name").equals("Piastri"); // chained filteration
+
+    user = await User.where("age")
+      .lt("40")
+      .where("name")
+      .equals("Puja")
+      .limit(2); // add limit to number of results
+
+    user = await User.where("age")
+      .lt("40")
+      .where("name")
+      .equals("Puja")
+      .limit(2)
+      .select("hobbies"); // return specific fields after filtering results
 
     console.log(user);
   } catch (error) {
     console.log(error.message);
   }
 }
-
 runMongoDB();
